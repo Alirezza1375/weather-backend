0f9635df96c773a08ab6804562ff37b0520ccbd7
Alirezza1375 <alirezagoudarzi1375@gmail.com>
2026-02-12 21:49:41 +0100

    router defined

---
+++ src/modules/weather/weather.routes.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { getWeatherByCity } from "./weather.controller.js";
+
+const router = Router();
+
+router.get("/:city", getWeatherByCity);
+
+export default router;

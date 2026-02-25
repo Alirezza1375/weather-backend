86d3e9284eee2f6156def08574328001173d124b
Alirezza1375 <alirezagoudarzi1375@gmail.com>
2026-02-25 14:15:38 +0100

    (feat) controller implementaion extracts request params and calls service

--- src/modules/weather/weather.controller.js
+++ src/modules/weather/weather.controller.js
@@ -2,14 +2,13 @@ import { fetchWeather } from "./weather.service.js";
 
 export const getWeatherByCity = async (req, res, next) => {
   try {
-    console.log("Controller hit", req.params);
     const { city } = req.params;
 
     const data = await fetchWeather(city);
 
     res.status(200).json(data);
   } catch (err) {
-    console.error("faild to get data", err);
+    console.error(err);
     next(err);
   }
 };

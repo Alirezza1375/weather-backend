62abd660338b69e91b8433a73316357723db5e03
Alirezza1375 <alirezagoudarzi1375@gmail.com>
2026-02-25 15:44:42 +0100

    (feat) mocking routes using jest and supertest

--- src/modules/weather/weather.routes.test.js
+++ src/modules/weather/weather.routes.test.js
@@ -1,12 +1,32 @@
 import request from "supertest";
-import app from "../../app.js";
-import { expect } from "@jest/globals";
+import { jest } from "@jest/globals";
+
+// MUST mock BEFORE importing app
+jest.unstable_mockModule("./weather.service.js", () => ({
+  fetchWeather: jest.fn(),
+}));
+
+const { fetchWeather } = await import("./weather.service.js");
+const { default: app } = await import("../../app.js");
 
 describe("GET /api/weather/:city", () => {
-  test("returns 200 and weather data", async () => {
+  test("returns 200 and formatted response", async () => {
+    fetchWeather.mockResolvedValue({
+      location: { city: "berlin" },
+      temperature: 10,
+    });
+
     const response = await request(app).get("/api/weather/berlin");
 
     expect(response.status).toBe(200);
-    expect(response.body.city).toBe("berlin");
+
+    // we test what the route returns
+    expect(response.body).toEqual({
+      location: { city: "berlin" },
+      temperature: 10,
+    });
+
+    // ensure service was called correctly
+    expect(fetchWeather).toHaveBeenCalledWith("berlin");
   });
 });

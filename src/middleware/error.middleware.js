5bc334f34a4735a13640c72cd73afc1be0e7ac30
Alirezza1375 <alirezagoudarzi1375@gmail.com>
2026-02-25 14:13:54 +0100

    (feat) global error handler using response error data

--- src/middleware/error.middleware.js
+++
@@ -1,8 +0,0 @@
-export const globalErrorHandler = (err, req, res, next) => {
-  console.log("Error:", err);
-
-  res.status(err.statusCode || 500).json({
-    status: err.status || "error",
-    message: err.message || "Something went wrong",
-  });
-};

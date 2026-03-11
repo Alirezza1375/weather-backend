a5024ffc40ebd70244742699945300ace6092ff0
Alirezza1375 <alirezagoudarzi1375@gmail.com>
2026-03-11 18:35:47 +0100

    feat: global error handler to handle global error

--- src/middleware/error.middleware.js
+++ src/middleware/error.middleware.js
@@ -3,6 +3,6 @@ export const globalErrorHandler = (err, req, res, next) => {
 
   res.status(err.statusCode || 500).json({
     status: err.status || "error",
-    message: err.message["Something went wrong"],
+    message: err.message || "Something went wrong",
   });
 };

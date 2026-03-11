2f9459325d848ddc9444668dedf24c22b455089d
Alirezza1375 <alirezagoudarzi1375@gmail.com>
2026-03-11 18:44:39 +0100

    feat: configure Express app with routing, middleware, and global error handling

--- src/app.js
+++ src/app.js
@@ -1,14 +1,22 @@
 import express from "express";
 import cors from "cors";
-import { globalErrorHandler } from "./middleware/error.middleware.js";
 import weatherRouter from "./modules/weather/weather.routes.js";
+import { globalErrorHandler } from "./middleware/error.middleware.js";
+import morgan from "morgan";
+import { apiLimiter } from "./middleware/rateLimiter.js";
+import healthRouter from "./modules/routes/health.routes.js";
 
 const app = express();
+
+app.use(morgan("dev"));
 
 app.use(cors());
 app.use(express.json());
 
+app.use("/api", apiLimiter);
+
 app.use("/api/weather", weatherRouter);
+app.use("/api/health", healthRouter);
 
 app.use(globalErrorHandler);
 

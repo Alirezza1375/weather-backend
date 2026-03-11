c3097baaa1560bcfe143c8ac9cece6b45cf8a066
Alirezza1375 <alirezagoudarzi1375@gmail.com>
2026-03-11 18:45:45 +0100

    feat: add server entry point to start Express app with environment config

--- src/server.js
+++ src/server.js
@@ -1,9 +1,6 @@
-import dotenv from "dotenv";
-dotenv.config();
 import app from "./app.js";
+import { env } from "./config/env.js";
 
-const PORT = process.env.PORT || 5001;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(env.PORT, () => {
+  console.log(`Server is running on port ${env.PORT}`);
 });
